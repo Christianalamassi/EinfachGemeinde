0c520c99a800d94f8c01c95b0f91825406e6720a
Christian <christianalamassi@gmail.com>
2026-06-13 10:18:44 +0200

    Add Render deployment files

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Pray
 from django.contrib import messages
+from .models import Pray
 
 
 def home(request):
@@ -8,23 +8,27 @@ def home(request):
 
 
 def pray(request):
-
     if request.method == 'POST':
         # Grab data from the form using the 'name' attributes
         email = request.POST.get('email')
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
 
-        # Save the data to the database
-        Pray.objects.create(
-            email=email,
-            first_name=first_name,
-            last_name=last_name
-        )
+        try:
+            # raise Exception("Testing my error style!")
+            # Save the data to the database
+            Pray.objects.create(
+                email=email,
+                first_name=first_name,
+                last_name=last_name
+            )
+            # Success message
+            messages.success(request, f"Thank you {first_name}, your request has been sent successfully!")
         
-       # Add the success message here
-        messages.success(request, f"Thank you {first_name}, your request has been sent successfully!")
-        
+        except Exception as e:
+            # Failure message if something goes wrong
+            messages.error(request, "Oops! Something went wrong. Please try submitting your request again.")
+
         # Redirect back to the form page safely
         return redirect('pray')
 
